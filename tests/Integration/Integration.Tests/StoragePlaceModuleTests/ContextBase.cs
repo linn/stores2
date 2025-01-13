34343fcfb6a290a06937189da74316bf92eee3c7
Richard Phillips <richard.phillips@linn.co.uk>
2025-01-13 16:16:16 +0000

    ok tests

--- tests/Integration/Integration.Tests/StoragePlaceModuleTests/ContextBase.cs
+++ tests/Integration/Integration.Tests/StoragePlaceModuleTests/ContextBase.cs
@@ -2,7 +2,10 @@
 {
     using System.Net.Http;
 
+    using Linn.Common.Pdf;
+    using Linn.Common.Rendering;
     using Linn.Common.Reporting.Resources.ResourceBuilders;
+    using Linn.Stores2.Domain.LinnApps.Models;
     using Linn.Stores2.Domain.LinnApps.Reports;
     using Linn.Stores2.Facade.Services;
     using Linn.Stores2.IoC;
@@ -24,14 +27,22 @@
 
         protected IStoragePlaceAuditReportService StoragePlaceAuditReportService { get; private set; }
 
+        protected IPdfService PdfService { get; private set; }
+
+        protected IHtmlTemplateService<StoragePlaceAuditReport> HtmlTemplateServiceForStorageAudit { get; private set; }
+
         [SetUp]
         public void SetUpContext()
         {
             this.StoragePlaceAuditReportService = Substitute.For<IStoragePlaceAuditReportService>();
+            this.HtmlTemplateServiceForStorageAudit = Substitute.For<IHtmlTemplateService<StoragePlaceAuditReport>>();
+            this.PdfService = Substitute.For<IPdfService>();
 
             this.StoragePlaceAuditReportFacadeService = new StoragePlaceAuditReportFacadeService(
                 this.StoragePlaceAuditReportService,
-                new ReportReturnResourceBuilder());
+                new ReportReturnResourceBuilder(),
+                this.PdfService,
+                this.HtmlTemplateServiceForStorageAudit);
 
             this.Client = TestClient.With<StoragePlaceModule>(
                 services =>
