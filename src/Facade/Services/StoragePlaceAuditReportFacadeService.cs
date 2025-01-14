34c540a5700d2b6dfca2a0f490356b1675cdde6f
Richard Phillips <richardiphillips@users.noreply.github.com>
2025-01-14 10:43:34 +0000

    storage place audit report client (#12)
    * add report display and pdf
    
    * ok tests
    
    * set range and tidy format
    
    * add options
    
    * remove
    
    * change to search
    
    * accept pallet as just number

--- src/Facade/Services/StoragePlaceAuditReportFacadeService.cs
+++ src/Facade/Services/StoragePlaceAuditReportFacadeService.cs
@@ -1,10 +1,15 @@
 ﻿namespace Linn.Stores2.Facade.Services
 {
     using System.Collections.Generic;
+    using System.IO;
+    using System.Threading.Tasks;
 
     using Linn.Common.Facade;
+    using Linn.Common.Pdf;
+    using Linn.Common.Rendering;
     using Linn.Common.Reporting.Resources.ReportResultResources;
     using Linn.Common.Reporting.Resources.ResourceBuilders;
+    using Linn.Stores2.Domain.LinnApps.Models;
     using Linn.Stores2.Domain.LinnApps.Reports;
 
     public class StoragePlaceAuditReportFacadeService : IStoragePlaceAuditReportFacadeService
@@ -13,12 +18,20 @@
 
         private readonly IReportReturnResourceBuilder resourceBuilder;
 
+        private readonly IPdfService pdfService;
+
+        private readonly IHtmlTemplateService<StoragePlaceAuditReport> htmlTemplateServiceForStorageAudit;
+
         public StoragePlaceAuditReportFacadeService(
             IStoragePlaceAuditReportService storagePlaceAuditReportService,
-            IReportReturnResourceBuilder resourceBuilder)
+            IReportReturnResourceBuilder resourceBuilder,
+            IPdfService pdfService,
+            IHtmlTemplateService<StoragePlaceAuditReport> htmlTemplateServiceForStorageAudit)
         {
             this.storagePlaceAuditReportService = storagePlaceAuditReportService;
             this.resourceBuilder = resourceBuilder;
+            this.pdfService = pdfService;
+            this.htmlTemplateServiceForStorageAudit = htmlTemplateServiceForStorageAudit;
         }
 
         public IResult<ReportReturnResource> GetStoragePlaceAuditReport(
@@ -28,6 +41,17 @@
             var result = this.storagePlaceAuditReportService.StoragePlaceAuditReport(locationList, locationRange);
             
             return new SuccessResult<ReportReturnResource>(this.resourceBuilder.Build(result));
+        }
+
+        public async Task<Stream> GetStoragePlaceAuditReportAsPdf(string[] locationList, string locationRange)
+        {
+            var result = this.storagePlaceAuditReportService.StoragePlaceAuditReport(locationList, locationRange);
+
+            var data = new StoragePlaceAuditReport(result);
+
+            var html = await this.htmlTemplateServiceForStorageAudit.GetHtml(data);
+
+            return await this.pdfService.ConvertHtmlToPdf(html, false);
         }
     }
 }
--- src/Service.Host/client/src/itemTypes.js
+++ src/Service.Host/client/src/itemTypes.js
@@ -3,7 +3,10 @@
 const itemTypes = {
     health: { url: `${config.appRoot}/healthcheck` },
     countries: { url: `${config.appRoot}/stores2/countries` },
-    carriers: { url: `${config.appRoot}/stores2/carriers` }
+    carriers: { url: `${config.appRoot}/stores2/carriers` },
+    storagePlaceAudit: { url: `${config.appRoot}/stores2/reports/storage-place-audit/report` },
+    storagePlaces: { url: `${config.proxyRoot}/inventory/storage-places` },
+    auditLocations: { url: `${config.proxyRoot}/inventory/audit-locations` }
 };
 
 export default itemTypes;
