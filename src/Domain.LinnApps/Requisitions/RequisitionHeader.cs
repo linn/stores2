cae79d347a77833dab9d057f1cf9c992e1b32533
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-01-14 10:08:47 +0000

    domain stuff

---
+++ src/Domain.LinnApps/Requisitions/RequisitionHeader.cs
@@ -0,0 +1,43 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Requisitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Linn.Stores2.Domain.LinnApps.Parts;
+    using Linn.Stores2.Domain.LinnApps.Stock;
+
+    public class RequisitionHeader
+    {
+        public int ReqNumber { get; protected init; }
+
+        public DateTime DateCreated { get; protected init; }
+
+        public int? Document1 { get; protected set; }
+
+        public IEnumerable<RequisitionLine> Lines { get; protected set; }
+
+        public IEnumerable<ReqMove> Moves { get; protected set; }
+
+        public decimal? Qty { get; protected set; }
+
+        public string Document1Name { get; protected set; }
+
+        public string PartNumber { get; protected set; }
+
+        public Part Part { get; protected set; }
+
+        public int? ToLocationId { get; protected set; }
+
+        public StorageLocation ToLocation { get; protected set; }
+
+        public string Cancelled { get; protected set; }
+
+        public int? CancelledBy { get; protected set; }
+
+        public DateTime? DateCancelled { get; protected set; }
+
+        public string CancelledReason { get; protected set; }
+
+        public string FunctionCode { get; protected set; }
+    }
+}
---
+++ src/Domain.LinnApps/Requisitions/RequisitionLine.cs
@@ -0,0 +1,28 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Requisitions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RequisitionLine
+    {
+        public int ReqNumber { get; protected init; }
+
+        public int LineNumber { get; protected init; }
+
+        public string PartNumber { get; protected set; }
+
+        public string TransactionCode { get; protected set; }
+
+        public StoresTransactionDefinition TransactionDefinition { get; protected set; }
+
+        public DateTime? DateCancelled { get; protected set; }
+
+        public string CancelledReason { get; protected set; }
+
+        public int? CancelledBy { get; protected set; }
+
+        public int? Document1Line { get; protected set; }
+
+        public IEnumerable<ReqMove> Moves { get; protected set; }
+    }
+}
