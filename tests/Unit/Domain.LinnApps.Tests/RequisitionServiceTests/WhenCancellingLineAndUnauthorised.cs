4e69f3c9fee868fbe1a24387b2f123398ca38a3f
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-01-27 12:56:01 +0000

    service tests

---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionServiceTests/WhenCancellingLineAndUnauthorised.cs
@@ -0,0 +1,38 @@
+namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionServiceTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using FluentAssertions;
+    
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenCancellingLineAndUnauthorised : ContextBase
+    {
+        private Func<Task> action;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            var user = new User
+                           {
+                               UserNumber = 33087,
+                               Privileges = new List<string>()
+                           };
+            
+            this.AuthService.HasPermissionFor(
+                AuthorisedActions.CancelRequisition, Arg.Any<IEnumerable<string>>())
+                .Returns(false);
+            this.action = async () => await this.Sut.CancelLine(123, 1, user, "REASON");
+        }
+
+        [Test]
+        public async Task ShouldThrow()
+        {
+            await this.action.Should().ThrowAsync<UnauthorizedAccessException>();
+        }
+    }
+}
