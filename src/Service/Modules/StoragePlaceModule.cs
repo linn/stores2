5dbc6538a9d2d2d766a3e2acf6096a04404d4413
OliverMclachlan <146720368+OliverMclachlan@users.noreply.github.com>
2025-01-28 09:34:36 +0000

    tidy up usings

--- src/Service/Modules/StoragePlaceModule.cs
+++ src/Service/Modules/StoragePlaceModule.cs
@@ -4,8 +4,6 @@ namespace Linn.Stores2.Service.Modules
 
     using Linn.Common.Service.Core;
     using Linn.Common.Service.Core.Extensions;
-    using Linn.Stores2.Domain.LinnApps.Stock;
-    using Linn.Stores2.Facade.Common;
     using Linn.Stores2.Facade.Services;
     using Linn.Stores2.Resources.RequestResources;
     using Linn.Stores2.Service.Models;
