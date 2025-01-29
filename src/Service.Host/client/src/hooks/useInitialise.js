c9263fd4160b6460a229daba283c3251cb575d64
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-01-29 15:52:10 +0000

    pass requiresAuth flag

--- src/Service.Host/client/src/hooks/useInitialise.js
+++ src/Service.Host/client/src/hooks/useInitialise.js
@@ -3,9 +3,9 @@ import useGet from './useGet';
 
 // performs a single GET request once, as soon as the calling component is first mounted
 // the url is specified by the below parameters, where id and queryString are optional
-function useInitialise(url, id, queryString) {
+function useInitialise(url, id, queryString, requiresAuth = false) {
     const [hasFetched, setHasFetched] = useState(false);
-    const { send, isLoading, errorMessage, result } = useGet(url, true);
+    const { send, isLoading, errorMessage, result } = useGet(url, requiresAuth);
 
     if (!hasFetched) {
         send(id, queryString);
