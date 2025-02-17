596f29e0379bad68479b5de06a3ec383970c2c36
OliverMclachlan <OliverM@linn.co.uk>
2025-02-17 15:37:00 +0000

    wip

--- src/Facade/Common/AsyncFacadeService.cs
+++ src/Facade/Common/AsyncFacadeService.cs
@@ -83,7 +83,7 @@
         {
             try
             {
-                var entity = this.repository.FindBy(this.FindExpression(searchResource));
+                var entity = await this.repository.FindByAsync(this.FindExpression(searchResource));
                 return new SuccessResult<TResource>(this.BuildResource(entity, privileges));
             }
             catch (NotImplementedException)
