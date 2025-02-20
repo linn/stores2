7b6fa737f8be6cfe980965c5256e6aac3eb10dc9
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-02-20 15:22:31 +0000

    tidy

--- src/Service.Host/client/src/components/requisitions/PendingRequisitions.js
+++ src/Service.Host/client/src/components/requisitions/PendingRequisitions.js
@@ -6,12 +6,7 @@ import queryString from 'query-string';
 import { DataGrid } from '@mui/x-data-grid';
 import Grid from '@mui/material/Grid2';
 import Button from '@mui/material/Button';
-import {
-    utilities,
-    InputField,
-    Dropdown,
-    Search
-} from '@linn-it/linn-form-components-library';
+import { utilities, Dropdown, Search } from '@linn-it/linn-form-components-library';
 import Page from '../Page';
 import config from '../../config';
 import itemTypes from '../../itemTypes';
@@ -42,14 +37,14 @@ function PendingRequisitions() {
     } = useSearch(itemTypes.historicEmployees.url, 'id', 'fullName', 'fullName', false, true);
 
     const [options, setOptions] = useState({ pending: true });
-    const [filters, setFilters] = useState({ 
-        accountingCompany: "LINN", 
+    const [filters, setFilters] = useState({
+        accountingCompany: 'LINN',
         functionCode: null,
         daysAgo: 0,
         docType: null,
         needsAuth: null,
-        department: null, 
-        departmentCode: null, 
+        department: null,
+        departmentCode: null,
         departmentName: null,
         employee: null,
         employeeId: null,
@@ -92,23 +87,23 @@ function PendingRequisitions() {
     const getReqs = () => {
         const query = queryString.stringify(options);
         send(null, `?${query}`);
-    }
+    };
 
     const clearFilters = () => {
-        setFilters({ 
-            accountingCompany: "LINN", 
+        setFilters({
+            accountingCompany: 'LINN',
             functionCode: null,
             docType: null,
             needsAuth: null,
             daysAgo: 0,
-            department: null, 
-            departmentCode: null, 
+            department: null,
+            departmentCode: null,
             departmentName: null,
             employee: null,
             employeeId: null,
             employeeName: null
         });
-    }
+    };
 
     const columns = [
         {
@@ -131,45 +126,57 @@ function PendingRequisitions() {
             return moment(dateString).isBefore(moment().subtract(filters.daysAgo, 'days'));
         }
         return true;
-    }
+    };
 
     const matchesDocType = documentName => {
         if (filters.docType) {
-            if (filters.docType.includes(',')) 
-            {
+            if (filters.docType.includes(',')) {
                 return filters.docType.includes(documentName);
             }
             return filters.docType === documentName;
         }
         return true;
-    }
+    };
 
     const matchesNeedsAuth = requiresAuthorisation => {
         if (filters.needsAuth !== null) {
             return filters.needsAuth ? requiresAuthorisation : !requiresAuthorisation;
         }
         return true;
-    }
+    };
 
     const filteredReqs = reqs => {
-        return reqs ? reqs.filter(r => (!filteredReqs.accountingCompany || r.accountingCompanyCode === filters.accountingCompany) 
-            && (!filters.departmentCode || !filters.department || r.department?.departmentCode === filters.departmentCode)
-            && (!filters.employeeId || !filters.employee || r.createdBy === filters.employeeId)
-            && (!filters.functionCode || r.storesFunction?.code === filters.functionCode)
-            && overDaysAgo(r.dateCreated) && matchesDocType(r.document1Name) && matchesNeedsAuth(r.requiresAuthorisation)
-        ) : [];
-    }
+        return reqs
+            ? reqs.filter(
+                  r =>
+                      (!filteredReqs.accountingCompany ||
+                          r.accountingCompanyCode === filters.accountingCompany) &&
+                      (!filters.departmentCode ||
+                          !filters.department ||
+                          r.department?.departmentCode === filters.departmentCode) &&
+                      (!filters.employeeId ||
+                          !filters.employee ||
+                          r.createdBy === filters.employeeId) &&
+                      (!filters.functionCode || r.storesFunction?.code === filters.functionCode) &&
+                      overDaysAgo(r.dateCreated) &&
+                      matchesDocType(r.document1Name) &&
+                      matchesNeedsAuth(r.requiresAuthorisation)
+              )
+            : [];
+    };
 
     const reqRows = () => {
-        return filteredReqs(result).map(r => ({
-            ...r,
-            id: r.reqNumber,
-            created: moment(r.dateCreated).format('DD-MMM-YYYY'),
-            department: r.department?.description,
-            functionCode: r.storesFunction?.code,
-            requiresAuth: r.requiresAuthorisation === true ? "Yes" : "No",
-            document1: `${r.document1Name}${r.document1 ? r.document1 : ''}`
-        })) || [];
+        return (
+            filteredReqs(result).map(r => ({
+                ...r,
+                id: r.reqNumber,
+                created: moment(r.dateCreated).format('DD-MMM-YYYY'),
+                department: r.department?.description,
+                functionCode: r.storesFunction?.code,
+                requiresAuth: r.requiresAuthorisation === true ? 'Yes' : 'No',
+                document1: `${r.document1Name}${r.document1 ? r.document1 : ''}`
+            })) || []
+        );
     };
 
     return (
@@ -178,7 +185,7 @@ function PendingRequisitions() {
                 <Grid size={11}>
                     <Typography variant="h4">Pending Requisitions</Typography>
                 </Grid>
-                <Grid size={1}/>
+                <Grid size={1} />
                 <Grid size={3}>
                     {accountingCompaniesResult && (
                         <Dropdown
@@ -235,7 +242,7 @@ function PendingRequisitions() {
                             { id: 'REQ', displayText: 'Requisition' },
                             { id: 'L', displayText: 'Loan' },
                             { id: 'WO', displayText: 'Works Order' },
-                            { id: 'PO', displayText: 'Purchase Order' },
+                            { id: 'PO', displayText: 'Purchase Order' }
                         ]}
                         allowNoValue
                     />
@@ -246,18 +253,22 @@ function PendingRequisitions() {
                         label="Department"
                         resultsInModal
                         resultLimit={100}
-                        helperText={filters.departmentName ? filters.departmentName : "Enter a search term and press enter to look up departments"}
+                        helperText={
+                            filters.departmentName
+                                ? filters.departmentName
+                                : 'Enter a search term and press enter to look up departments'
+                        }
                         value={filters.department}
                         handleValueChange={(_, newVal) => {
-                            handleFiltersChange("department", newVal );
+                            handleFiltersChange('department', newVal);
                         }}
                         search={searchDepartments}
                         loading={departmentsSearchLoading}
                         searchResults={departmentsSearchResults}
                         priorityFunction="closestMatchesFirst"
                         onResultSelect={r => {
-                            handleFiltersChange("departmentCode", r?.departmentCode);
-                            handleFiltersChange("departmentName", r?.description);
+                            handleFiltersChange('departmentCode', r?.departmentCode);
+                            handleFiltersChange('departmentName', r?.description);
                         }}
                         clearSearch={clearDepartmentsSearch}
                         autoFocus={false}
@@ -269,18 +280,22 @@ function PendingRequisitions() {
                         label="Employee"
                         resultsInModal
                         resultLimit={100}
-                        helperText={filters.employeeName ? filters.employeeName : "Enter a search term and press enter to look up employees"}
+                        helperText={
+                            filters.employeeName
+                                ? filters.employeeName
+                                : 'Enter a search term and press enter to look up employees'
+                        }
                         value={filters.employee}
                         handleValueChange={(_, newVal) => {
-                            handleFiltersChange("employee", newVal );
+                            handleFiltersChange('employee', newVal);
                         }}
                         search={searchEmployees}
                         loading={employeesSearchLoading}
                         searchResults={employeesSearchResults}
                         priorityFunction="closestMatchesFirst"
                         onResultSelect={r => {
-                            handleFiltersChange("employeeId", r?.id);
-                            handleFiltersChange("employeeName", r?.description);
+                            handleFiltersChange('employeeId', r?.id);
+                            handleFiltersChange('employeeName', r?.description);
                         }}
                         clearSearch={clearEmployeesSearch}
                         autoFocus={false}
