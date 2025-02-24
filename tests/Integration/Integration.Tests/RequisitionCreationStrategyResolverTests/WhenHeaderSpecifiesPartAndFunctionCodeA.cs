5227a8785d67907900efc649ea4f741666971423
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-02-24 14:28:52 +0000

    resolver tests

---
+++ tests/Integration/Integration.Tests/RequisitionCreationStrategyResolverTests/WhenHeaderSpecifiesPartAndFunctionCodeA.cs
@@ -0,0 +1,23 @@
+﻿namespace Linn.Stores2.Integration.Tests.RequisitionCreationStrategyResolverTests
+{
+    using FluentAssertions;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.Domain.LinnApps.Requisitions.CreationStrategies;
+    using NUnit.Framework;
+
+    public class WhenHeaderSpecifiesPartAndFunctionCodeA : ContextBase
+    {
+        [Test]
+        public void ShouldResolveCorrectStrategy()
+        {
+            var context = new RequisitionCreationContext
+                              {
+                                  Function = new StoresFunction() { FunctionType = "A" },
+                                  PartNumber = "PART"
+                              };
+            var result = this.Sut.Resolve(context);
+
+            result.Should().BeOfType<AutomaticBookFromHeaderStrategy>();
+        }
+    }
+}
