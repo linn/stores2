810412db08e1ba4d6e2feb8caee3cc65648bac70
Ross Stewart <ross.stewart@linn.co.uk>
2025-02-28 10:04:12 +0000

    contextbase

---
+++ tests/Unit/Domain.LinnApps.Tests/GoodsInLogTests/ContextBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.GoodsInLogTests
+{
+    using Linn.Common.Persistence;
+    using Linn.Common.Reporting.Models;
+    using Linn.Stores2.Domain.LinnApps.GoodsIn;
+    using Linn.Stores2.Domain.LinnApps.Reports;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class ContextBase
+    {
+        protected GoodsInLogReportService Sut { get; set; }
+
+        protected IRepository<GoodsInLogEntry, int> GoodsInLogRepository { get; private set; }
+
+        protected IReportingHelper ReportingHelper { get; private set; }
+
+        [SetUp]
+        public void SetUpContext()
+        {
+            this.GoodsInLogRepository = Substitute.For<IRepository<GoodsInLogEntry, int>>();
+            this.ReportingHelper = new ReportingHelper();
+            this.Sut = new GoodsInLogReportService(
+                this.ReportingHelper,
+                this.GoodsInLogRepository);
+        }
+    }
+}
