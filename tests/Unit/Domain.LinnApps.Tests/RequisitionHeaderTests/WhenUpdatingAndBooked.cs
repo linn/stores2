baa76e04719cf9e76527a6eacd243920268e9c3a
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-03-12 14:47:39 +0000

    restrict updates when booked

---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionHeaderTests/WhenUpdatingAndBooked.cs
@@ -0,0 +1,43 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionHeaderTests
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Linn.Stores2.Domain.LinnApps.Accounts;
+    using Linn.Stores2.Domain.LinnApps.Exceptions;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.TestData.Requisitions;
+
+    using NUnit.Framework;
+
+    public class WhenUpdatingAndBooked
+    {
+        private Action action;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var req = new RequisitionHeader(
+                new Employee(),
+                new StoresFunction { FunctionCode = "F1" },
+                "F",
+                12345678,
+                "TYPE",
+                new Department(),
+                new Nominal(),
+                reference: "Goodbye Reqs");
+            req.AddLine(new LineWithMoves(1, 1));
+            req.Book(new Employee());
+
+            this.action = () => req.Update("new comment");
+        }
+
+        [Test]
+        public void ShouldThrow()
+        {
+            this.action.Should().Throw<RequisitionException>()
+                .WithMessage("Cannot amend a booked req");
+        }
+    }
+}
