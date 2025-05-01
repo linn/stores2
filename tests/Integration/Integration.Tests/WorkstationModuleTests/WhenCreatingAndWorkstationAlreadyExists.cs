b7bcd9ce78644e103be4f622fc71a7716bd6c955
Ross Stewart <ross.stewart@linn.co.uk>
2025-05-01 15:30:23 +0100

    tests

---
+++ tests/Integration/Integration.Tests/WorkstationModuleTests/WhenCreatingAndWorkstationAlreadyExists.cs
@@ -0,0 +1,82 @@
+﻿namespace Linn.Stores2.Integration.Tests.WorkstationModuleTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http.Json;
+
+    using FluentAssertions;
+
+    using Linn.Stores2.Domain.LinnApps.Stores;
+    using Linn.Stores2.Integration.Tests.Extensions;
+    using Linn.Stores2.Resources.Stores;
+
+    using NUnit.Framework;
+
+    public class WhenCreatingAndCodeAlreadyExists : ContextBase
+    {
+        private WorkstationResource createResource;
+
+        private Workstation workstation;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.workstation = new Workstation(
+                "Test",
+                "description",
+                new Cit
+                    {
+                        Code = "R",
+                        Name = "R CODE"
+                    },
+                "V",
+                "Z",
+                new List<WorkstationElement>
+                    {
+                    });
+            this.DbContext.Workstations.AddAndSave(this.DbContext, this.workstation);
+
+            this.createResource = new WorkstationResource
+            {
+                WorkstationCode = "Test",
+                CitCode = "R",
+                CitName = "R CODE",
+                Description = "A TEST WORKSTATION",
+                ZoneType = "Z",
+                VaxWorkstation = "161",
+                WorkstationElements = new List<WorkstationElementResource>
+                                                                    {
+                                                                        new WorkstationElementResource
+                                                                            {
+                                                                                WorkstationCode = "WORKSTATIONCODE",
+                                                                                CreatedBy = 33156,
+                                                                                CreatedByName = "RSTEWART",
+                                                                                DateCreated = DateTime.Today.ToString("o"),
+                                                                                LocationId = 123,
+                                                                                PalletNumber = 567,
+                                                                                WorkstationElementId = 1
+                                                                            },
+                                                                        new WorkstationElementResource
+                                                                            {
+                                                                                WorkstationCode = "WORKSTATIONCODE",
+                                                                                CreatedBy = 33156,
+                                                                                CreatedByName = "RSTEWART",
+                                                                                DateCreated = DateTime.Today.ToString("o"),
+                                                                                LocationId = 567,
+                                                                                PalletNumber = 890,
+                                                                                WorkstationElementId = 2
+                                                                            }
+                                                                    }
+            };
+
+            this.Response = this.Client.PostAsJsonAsync($"/stores2/work-stations", this.createResource).Result;
+        }
+
+        [Test]
+        public void ShouldReturnCreated()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ tests/Integration/Integration.Tests/WorkstationModuleTests/WhenGettingAll.cs
@@ -0,0 +1,67 @@
+﻿namespace Linn.Stores2.Integration.Tests.WorkstationModuleTests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+
+    using FluentAssertions;
+
+    using Linn.Stores2.Domain.LinnApps.Stores;
+    using Linn.Stores2.Integration.Tests.Extensions;
+    using Linn.Stores2.Resources.Stores;
+
+    using NUnit.Framework;
+
+    public class WhenGettingAll : ContextBase
+    {
+        private Workstation workstation;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.workstation = new Workstation(
+                "Test",
+                "description",
+                new Cit
+                    {
+                        Code = "R",
+                        Name = "R CODE"
+                    },
+                "V",
+                "Z",
+                new List<WorkstationElement>
+                    {
+                    });
+
+            this.DbContext.Workstations.AddAndSave(this.DbContext, this.workstation);
+
+            this.Response = this.Client.Get(
+                "/stores2/work-stations/",
+                with =>
+                    {
+                        with.Accept("application/json");
+                    }).Result;
+        }
+
+        [Test]
+        public void ShouldReturnOk()
+        {
+            this.Response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void ShouldReturnJsonContentType()
+        {
+            this.Response.Content.Headers.ContentType.Should().NotBeNull();
+            this.Response.Content.Headers.ContentType?.ToString().Should().Be("application/json");
+        }
+
+        [Test]
+        public void ShouldReturnJsonBody()
+        {
+            var resource = this.Response.DeserializeBody<IEnumerable<WorkstationResource>>();
+            resource.First().WorkstationCode.Should().Be("test");
+            resource.First().Description.Should().Be("description");
+        }
+    }
+}
