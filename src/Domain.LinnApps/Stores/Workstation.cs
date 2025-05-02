d52cb02494b328c6f4608e052985a30f163cc38b
Ross Stewart <ross.stewart@linn.co.uk>
2025-05-02 09:53:18 +0100

    tests passing

--- src/Domain.LinnApps/Stores/Workstation.cs
+++ src/Domain.LinnApps/Stores/Workstation.cs
@@ -14,7 +14,7 @@
             Cit cit,
             string vaxWorkstation,
             string zoneType,
-            ICollection<WorkstationElement> elements)
+            IEnumerable<WorkstationElement> elements)
         {
             this.WorkstationCode = workstationCode;
             this.Description = description;
@@ -34,7 +34,7 @@
 
         public string ZoneType { get; set; }
 
-        public ICollection<WorkstationElement> WorkstationElements { get; set; }
+        public IEnumerable<WorkstationElement> WorkstationElements { get; set; }
 
         public void Update(
             string workstationCode,
@@ -42,7 +42,7 @@
             Cit cit,
             string vaxWorkstation,
             string zoneType,
-            ICollection<WorkstationElement> elements)
+            IEnumerable<WorkstationElement> elements)
         {
             this.WorkstationCode = workstationCode;
             this.Description = description;
