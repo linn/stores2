96269e8f46f7a0297ebd8375c0b9ce61f66bc26c
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2025-05-15 14:26:01 +0100

    herd stray using back into the pen

--- src/Facade/Services/RequisitionFacadeService.cs
+++ src/Facade/Services/RequisitionFacadeService.cs
@@ -1,6 +1,4 @@
-﻿using Linn.Stores2.Domain.LinnApps;
-
-namespace Linn.Stores2.Facade.Services
+﻿namespace Linn.Stores2.Facade.Services
 {
     using System;
     using System.Collections.Generic;
@@ -11,6 +9,7 @@ namespace Linn.Stores2.Facade.Services
     using Linn.Common.Domain.Exceptions;
     using Linn.Common.Facade;
     using Linn.Common.Persistence;
+    using Linn.Stores2.Domain.LinnApps;
     using Linn.Stores2.Domain.LinnApps.Requisitions;
     using Linn.Stores2.Facade.Common;
     using Linn.Stores2.Resources.Requisitions;
