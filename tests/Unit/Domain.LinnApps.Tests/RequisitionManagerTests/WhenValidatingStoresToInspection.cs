3ef62ffcbfce6847ec957a4a66aa261d4e860144
Andrew Fraser <andrewfraser73@hotmail.com>
2025-05-15 12:28:18 +0100

    make STGII work required a slight tweak to Default From Stock

---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionManagerTests/WhenValidatingStoresToInspection.cs
@@ -0,0 +1,68 @@
+﻿using Linn.Common.Domain;
+using Linn.Stores2.Domain.LinnApps.External;
+using Linn.Stores2.Domain.LinnApps.Parts;
+using Linn.Stores2.Domain.LinnApps.Requisitions;
+using Linn.Stores2.Domain.LinnApps.Stock;
+using Linn.Stores2.TestData.FunctionCodes;
+using Linn.Stores2.TestData.Transactions;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System;
+using FluentAssertions;
+using Linn.Stores2.TestData.Parts;
+
+namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionManagerTests
+{
+    public class WhenValidatingStoresToInspection : ContextBase
+    {
+        private RequisitionHeader result;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.EmployeeRepository.FindByIdAsync(33087).Returns(new Employee());
+            this.StoresFunctionRepository.FindByIdAsync(TestFunctionCodes.StoresToInspection.FunctionCode)
+                .Returns(TestFunctionCodes.StoresToInspection);
+            this.ReqStoredProcedures.CanPutPartOnPallet(TestParts.Cap003.PartNumber, 503).Returns(true);
+            this.PartRepository.FindByIdAsync(TestParts.Cap003.PartNumber).Returns(TestParts.Cap003);
+            this.TransactionDefinitionRepository.FindByIdAsync(TestTransDefs.StoresToInspection.TransactionCode)
+                .Returns(TestTransDefs.StoresToInspection);
+            this.PalletRepository.FindByIdAsync(503).Returns(new StoresPallet());
+            this.StateRepository.FindByIdAsync("STORES").Returns(new StockState("STORES", "Stores"));
+            this.StateRepository.FindByIdAsync("FAIL").Returns(new StockState("FAIL", "Fail"));
+            this.StockPoolRepository.FindByIdAsync("LINN").Returns(new StockPool());
+            this.StoresService.ValidOntoLocation(
+                Arg.Any<Part>(),
+                Arg.Any<StorageLocation>(),
+                Arg.Any<StoresPallet>(),
+                Arg.Any<StockState>()).Returns(new ProcessResult(true, null));
+            this.StockService.ValidStockLocation(null, 500, TestParts.Cap003.PartNumber, 1, "STORES").Returns(new ProcessResult(true, null));
+            this.result = await this.Sut.Validate(
+                33087,
+                TestFunctionCodes.StoresToInspection.FunctionCode,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                partNumber: TestParts.Cap003.PartNumber,
+                quantity: 1,
+                fromState: "STORES",
+                toState: "FAIL",
+                fromPalletNumber: 500,
+                toPalletNumber: 503,
+                fromStockPool: "LINN",
+                toStockPool: "LINN");
+        }
+
+        [Test]
+        public void ShouldReturnValidated()
+        {
+            this.result.Should().NotBeNull();
+        }
+    }
+}
---
+++ tests/Unit/Domain.LinnApps.Tests/StoresFunctionTests/WhenGettingDefaultFromStateForSTGII.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Linn.Stores2.Domain.LinnApps.Requisitions;
+using Linn.Stores2.Domain.LinnApps.Stores;
+using Linn.Stores2.TestData.Transactions;
+using NUnit.Framework;
+
+namespace Linn.Stores2.Domain.LinnApps.Tests.StoresFunctionTests
+{
+    public class WhenGettingDefaultFromStateForSTGII
+    {
+        private StoresFunction sut;
+
+        private string result;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.sut = new StoresFunction("STGII")
+            {
+                Description = "ISSUE PARTS FROM STORES TO INSPECTION FOR CHECKING",
+                BatchRequired = "N",
+                DepartmentNominalRequired = "N",
+                Document1RequiredFlag = "N",
+                Document1LineRequiredFlag = "N",
+                FromStockPoolRequired = "O",
+                FromStateRequired = "O",
+                FunctionType = "A",
+                ManualPickRequired = "M",
+                PartSource = "IP",
+                ProcessStage = 1,
+                ToLocationRequired = "O",
+                ToStateRequired = "O",
+                CanBeReversed = "N",
+                TransactionsTypes = new List<StoresFunctionTransaction>
+                {
+                    new StoresFunctionTransaction
+                    {
+                        FunctionCode = "STGII",
+                        TransactionDefinition =             new StoresTransactionDefinition
+                        {
+                            TransactionCode = "STGII",
+                            Description = "ISSUE PARTS FROM STORES TO INSPECTION FOR CHECKING",
+                            StockAllocations = "N",
+                            OntoTransactions = "Y",
+                            DecrementTransaction = "N",
+                            TakePriceFrom = "P",
+                            RequiresAuth = "N",
+                            FromState = "STORES",
+                            InspectedState = "FAIL",
+                            StoresTransactionPostings = new List<StoresTransactionPosting>
+                            {
+                                new StoresTransactionPosting("STGII", "C", null),
+                                new StoresTransactionPosting("STGII", "D", null)
+                            },
+                            StoresTransactionStates = new List<StoresTransactionState>
+                            {
+                                new StoresTransactionState("O", "STGII", "FAIL"),
+                                new StoresTransactionState("O", "STGII", "QC"),
+                                new StoresTransactionState("F", "STGII", "STORES"),
+                            }
+                        },
+                        TransactionCode = "STGII"
+                    }
+                }
+            };
+
+            this.result = this.sut.DefaultFromState();
+        }
+
+        [Test]
+        public void ShouldHaveDefaultFromState()
+        {
+            this.result.Should().Be("STORES");
+        }
+    }
+}
