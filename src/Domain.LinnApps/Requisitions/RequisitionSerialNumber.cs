1ad143d36dfaeabfd46c8cfb65ac970a4b177955
Andrew Fraser <andrewfraser73@hotmail.com>
2025-05-19 14:28:41 +0100

    pr feedback

--- src/Domain.LinnApps/Requisitions/RequisitionSerialNumber.cs
+++ src/Domain.LinnApps/Requisitions/RequisitionSerialNumber.cs
@@ -4,7 +4,6 @@
     {
         public RequisitionSerialNumber()
         {
-            
         }
 
         public RequisitionSerialNumber(
---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionLineTests/WhenAddingDuplicateRequisitionSerialNumber.cs
@@ -0,0 +1,38 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionLineTests
+{
+    using System.Threading.Tasks;
+    using System;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.Domain.LinnApps.Parts;
+    using NUnit.Framework;
+    using FluentAssertions;
+    using Linn.Stores2.Domain.LinnApps.Exceptions;
+
+    public class WhenAddingDuplicateRequisitionSerialNumber
+    {
+        private RequisitionLine sut;
+
+        private Action action;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.sut = new RequisitionLine(
+                123,
+                1,
+                new Part(),
+                10,
+                new StoresTransactionDefinition());
+
+            this.sut.AddSerialNumber(1);
+
+            this.action = () => this.sut.AddSerialNumber(1);
+        }
+
+        [Test]
+        public void ShouldThrow()
+        {
+            this.action.Should().Throw<RequisitionException>().WithMessage("Trying to add duplicate serial number 1 to line 1");
+        }
+    }
+}
