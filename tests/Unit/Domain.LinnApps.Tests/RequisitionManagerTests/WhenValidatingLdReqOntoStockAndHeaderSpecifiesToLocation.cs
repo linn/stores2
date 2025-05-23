95dd51a6a70ba79249046a8baa73ff42fd811ba9
Lewis <lewis.renfrew@linn.co.uk>
2025-05-23 11:05:52 +0100

    test

---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionManagerTests/WhenValidatingLdReqOntoStockAndHeaderSpecifiesToLocation.cs
@@ -0,0 +1,65 @@
+namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionManagerTests
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using FluentAssertions;
+
+    using Linn.Stores2.Domain.LinnApps.Accounts;
+    using Linn.Stores2.Domain.LinnApps.Parts;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.Domain.LinnApps.Stock;
+    using Linn.Stores2.TestData.FunctionCodes;
+    using Linn.Stores2.TestData.Transactions;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenValidatingLdreqOntoStockAndHeaderSpecifiesToLocation : ContextBase
+    {
+        private RequisitionHeader result;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            this.DepartmentRepository.FindByIdAsync("1607")
+                .Returns(new Department("1607", "DESC"));
+            this.NominalRepository.FindByIdAsync("2963")
+                .Returns(new Nominal("2963", "DESC"));
+            this.EmployeeRepository.FindByIdAsync(33087).Returns(new Employee());
+            this.StoresFunctionRepository.FindByIdAsync(TestFunctionCodes.LinnDeptReq.FunctionCode)
+                .Returns(TestFunctionCodes.LinnDeptReq);
+            this.TransactionDefinitionRepository.FindByIdAsync(TestTransDefs.LinnDeptToStock.TransactionCode)
+                .Returns(TestTransDefs.LinnDeptToStock);
+            this.PalletRepository.FindByIdAsync(123).Returns(new StoresPallet());
+            this.PartRepository.FindByIdAsync("PART").Returns(new Part());
+            this.ReqStoredProcedures.CanPutPartOnPallet("PART", 123).Returns(true);
+            this.result = await this.Sut.Validate(
+                33087,
+                TestFunctionCodes.LinnDeptReq.FunctionCode,
+                "O",
+                null,
+                null,
+                "1607",
+                "2963",
+                toPalletNumber: 123,
+                lines: new List<LineCandidate>
+                           {
+                               new LineCandidate
+                                   {
+                                       PartNumber = "PART",
+                                       Qty = 1,
+                                       TransactionDefinition = TestTransDefs.LinnDeptToStock.TransactionCode,
+                                       Moves = null // since header specifies toPalletNumber = 123 for all lines
+                                   }
+                           });
+        }
+
+        [Test]
+        public void ShouldReturnValidated()
+        {
+            this.result.Should().NotBeNull();
+        }
+    }
+}
