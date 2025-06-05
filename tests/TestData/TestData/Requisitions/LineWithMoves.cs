5ba8fc762059992249b9f0804b950ef8b575fad2
Richard Phillips <richardiphillips@users.noreply.github.com>
2025-06-05 15:55:58 +0100

    Cost of requisition report (#238)
    * add module
    
    * cost report back end
    
    * client
    
    * show message
    
    * remove pdf option for now
    
    * save file

--- tests/TestData/TestData/Requisitions/LineWithMoves.cs
+++ tests/TestData/TestData/Requisitions/LineWithMoves.cs
@@ -2,6 +2,7 @@ namespace Linn.Stores2.TestData.Requisitions
 {
     using Linn.Stores2.Domain.LinnApps.Parts;
     using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.Domain.LinnApps.Stores;
 
     public class LineWithMoves : RequisitionLine
     {
@@ -10,14 +11,21 @@ namespace Linn.Stores2.TestData.Requisitions
             this.Moves = new List<ReqMove> { new ReqMove() };
         }
 
-        public LineWithMoves(int reqNumber, int lineNumber, StoresTransactionDefinition  transactionDefinition)
+        public LineWithMoves(
+            int reqNumber,
+            int lineNumber,
+            StoresTransactionDefinition transactionDefinition,
+            decimal? quantity = null,
+            Part part = null,
+            ICollection<StoresBudget> budgets = null)
         {
             this.TransactionDefinition = transactionDefinition;
             this.ReqNumber = reqNumber;
             this.LineNumber = lineNumber;
-            this.Part = new Part();
-            this.Qty = 1;
+            this.Part = part ?? new Part();
+            this.Qty = quantity ?? 1;
             this.Moves = new List<ReqMove>();
+            this.StoresBudgets = budgets;
         }
     }
 }
---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionReportServiceTests/ContextBase.cs
@@ -0,0 +1,28 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionReportServiceTests
+{
+    using Linn.Common.Persistence;
+    using Linn.Common.Reporting.Models;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class ContextBase
+    {
+        protected RequisitionReportService Sut { get; set; }
+
+        protected IRepository<RequisitionHeader, int> RequisitionRepository { get; private set; }
+
+        protected IReportingHelper ReportingHelper { get; private set; }
+
+        [SetUp]
+        public void SetUpContext()
+        {
+            this.RequisitionRepository = Substitute.For<IRepository<RequisitionHeader, int>>();
+            this.ReportingHelper = new ReportingHelper();
+
+            this.Sut = new RequisitionReportService(this.RequisitionRepository, this.ReportingHelper);
+        }
+    }
+}
