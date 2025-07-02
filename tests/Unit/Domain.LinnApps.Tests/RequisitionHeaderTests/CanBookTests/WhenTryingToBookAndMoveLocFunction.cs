b44a0181f8c1811b4c46e8c6f46277c0da0065f3
Andrew Fraser <andrewfraser73@hotmail.com>
2025-07-02 12:36:59 +0100

    fix moveloc canbook bug (#289)

---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionHeaderTests/CanBookTests/WhenTryingToBookAndMoveLocFunction.cs
@@ -0,0 +1,36 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionHeaderTests.CanBookTests
+{
+    using FluentAssertions;
+    using Linn.Stores2.Domain.LinnApps.Accounts;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.TestData.FunctionCodes;
+    using NUnit.Framework;
+
+    public class WhenTryingToBookAndMoveLocFunction : CanBookContextBase
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            this.Sut = new RequisitionHeader(
+                new Employee(),
+                TestFunctionCodes.MoveLocation,
+                "F",
+                12345678,
+                "TYPE",
+                new Department(),
+                new Nominal(),
+                reference: null,
+                comments: "shipping",
+                fromPalletNumber: 1742,
+                toPalletNumber: 1740);
+
+            this.Result = this.Sut.RequisitionCanBeBooked();
+        }
+
+        [Test]
+        public void ShouldBeAbleToBook()
+        {
+            this.Result.Success.Should().BeTrue();
+        }
+    }
+}
