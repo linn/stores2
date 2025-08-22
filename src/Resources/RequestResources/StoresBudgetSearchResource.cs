d362e1ce38a29d57a24dedbeb93b1e86f5c8c56b
Richard Phillips <richardiphillips@users.noreply.github.com>
2025-08-22 14:45:34 +0100

    add new budget search (#306)

---
+++ src/Resources/RequestResources/StoresBudgetSearchResource.cs
@@ -0,0 +1,13 @@
+﻿namespace Linn.Stores2.Resources.RequestResources
+{
+    public class StoresBudgetSearchResource
+    {
+        public string PartNumber { get; set; }
+
+        public string FromDate { get; set; }
+        
+        public string ToDate { get; set; }
+
+        public string PartNumberStartsWith { get; set; }
+    }
+}
