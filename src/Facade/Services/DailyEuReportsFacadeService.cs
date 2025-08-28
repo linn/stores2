74610362ece77133d245de2b0ad6e81acbb5912b
OliverMclachlan <OliverM@linn.co.uk>
2025-08-28 14:41:24 +0100

    module & facae service

---
+++ src/Facade/Services/DailyEuReportsFacadeService.cs
@@ -0,0 +1,33 @@
+﻿using Linn.Common.Facade;
+using Linn.Common.Pdf;
+using Linn.Common.Reporting.Resources.ReportResultResources;
+using Linn.Common.Reporting.Resources.ResourceBuilders;
+using Linn.Stores2.Domain.LinnApps.Requisitions;
+using System;
+using System.Threading.Tasks;
+
+namespace Linn.Stores2.Facade.Services
+{
+    internal class DailyEuReportsFacadeService : IDailyEuReportFacdeService
+    {
+        public RequisitionReportFacadeService(
+            IRequisitionReportService requisitionReportService,
+            IReportReturnResourceBuilder reportResourceBuilder,
+            IPdfService pdfService)
+        {
+            this.requisitionReportService = requisitionReportService;
+            this.reportResourceBuilder = reportResourceBuilder;
+            this.pdfService = pdfService;
+        }
+
+        public Task<IResult<ReportReturnResource>> GetDailyEuDespatchReport(string fromDate, string toDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult<ReportReturnResource>> GetDailyEuImportRsnReport(string fromDate, string toDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Service/Modules/DailyEuReportsModule.cs
@@ -0,0 +1,49 @@
+﻿namespace Linn.Stores2.Service.Modules
+{
+    using System.Net;
+    using System.Threading.Tasks;
+
+    using Linn.Common.Service.Core;
+    using Linn.Common.Service.Core.Extensions;
+    using Linn.Stores2.Facade.Services;
+    using Linn.Stores2.Service.Models;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Routing;
+
+    public class DailyEuReportsModule : IModule
+    {
+        public void MapEndpoints(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/stores2/customs/daily/eu/import/rsn/report", this.DailyEuImportRsnReport);
+            app.MapGet("/stores2/customs/daily/eu/despatch/report", this.DailyEuDespatchReport);
+            app.MapGet("/requisitions/reports/requisition-cost", this.GetApp);
+        }
+
+        private async Task DailyEuImportRsnReport(
+            HttpRequest _,
+            HttpResponse res,
+            string fromDate,
+            string toDate,
+            IDailyEuReportFacdeService facadeService)
+        {
+            await res.Negotiate(await facadeService.GetDailyEuImportRsnReport(fromDate, toDate));
+        }
+
+        private async Task DailyEuDespatchReport(
+            HttpRequest _,
+            HttpResponse res,
+            string fromDate,
+            string toDate,
+            IDailyEuReportFacdeService facadeService)
+        {
+            var result = await facadeService.GetDailyEuDespatchReport(fromDate, toDate);
+        }
+
+        private async Task GetApp(HttpRequest req, HttpResponse res)
+        {
+            await res.Negotiate(new ViewResponse { ViewName = "Index.cshtml" });
+        }
+    }
+}
