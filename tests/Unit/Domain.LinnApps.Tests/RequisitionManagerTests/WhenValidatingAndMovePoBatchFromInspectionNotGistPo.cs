6201f39964b64b753c05bec5b372d41f82fff84f
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2025-11-17 17:10:05 +0000

    Better qc protect (#323)
    * idea - but doesn't really cover cases where batch ref is emptty since the system might try and get P-batch anyway
    
    * force specify batch ref if move from E-INS-LOC
    
    * test
    
    * missing tests

---
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionManagerTests/WhenValidatingAndMovePoBatchFromInspectionNotGistPo.cs
@@ -0,0 +1,74 @@
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionManagerTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using FluentAssertions;
+
+    using Linn.Common.Domain;
+    using Linn.Stores2.Domain.LinnApps.Accounts;
+    using Linn.Stores2.Domain.LinnApps.Exceptions;
+    using Linn.Stores2.Domain.LinnApps.Parts;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.TestData.FunctionCodes;
+    using Linn.Stores2.TestData.Transactions;
+
+    using NSubstitute;
+
+    using NUnit.Framework;
+
+    public class WhenValidatingAndMovePoBatchFromInspectionNotGistPo : ContextBase
+    {
+        private Func<Task> action;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.DepartmentRepository.FindByIdAsync("1607")
+                .Returns(new Department("1607", "DESC"));
+            this.NominalRepository.FindByIdAsync("2963")
+                .Returns(new Nominal("2963", "DESC"));
+            this.EmployeeRepository.FindByIdAsync(33087).Returns(new Employee());
+            this.StoresFunctionRepository.FindByIdAsync(TestFunctionCodes.LinnDeptReq.FunctionCode)
+                .Returns(TestFunctionCodes.LinnDeptReq);
+            var part = new Part { PartNumber = "PART" };
+            this.PartRepository.FindByIdAsync(part.PartNumber).Returns(part);
+            this.TransactionDefinitionRepository.FindByIdAsync(TestTransDefs.LinnDeptToStock.TransactionCode)
+                .Returns(TestTransDefs.LinnDeptToStock);
+            this.StoresService.ValidNominalAccount("1607", "2963")
+                .Returns(new NominalAccount());
+
+            this.action = () => this.Sut.Validate(
+                33087,
+                TestFunctionCodes.LinnDeptReq.FunctionCode,
+                "O",
+                null,
+                null,
+                "1607",
+                "2963",
+                lines: new List<LineCandidate>
+                           {
+                               new LineCandidate
+                                   {
+                                       PartNumber = part.PartNumber,
+                                       Qty = 1,
+                                       TransactionDefinition = TestTransDefs.LinnDeptToStock.TransactionCode,
+                                       Moves = new List<MoveSpecification>
+                                                   {
+                                                       new MoveSpecification { Qty = 1, ToPallet = 2345, FromLocation = "E-INS-LOC" }
+                                                   }
+                                   }
+                           },
+                batchRef: "P1234567");
+        }
+
+        [Test]
+        public async Task ShouldThrowException()
+        {
+            await this.action.Should()
+                .ThrowAsync<RequisitionException>().WithMessage(
+                    "Cannot move P-batch stock from the inspection location unless it is a GIST PO");
+        }
+    }
+}
