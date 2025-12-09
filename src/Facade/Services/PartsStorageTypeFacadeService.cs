8f11043815f3b374e46f88ff019f5730010d0b9d
OliverMclachlan <OliverM@linn.co.uk>
2025-12-09 12:35:12 +0000

    marks changes added

--- src/Facade/Services/PartsStorageTypeFacadeService.cs
+++ src/Facade/Services/PartsStorageTypeFacadeService.cs
@@ -121,8 +121,8 @@
         protected override Expression<Func<PartsStorageType, bool>> FilterExpression(PartsStorageTypeResource searchResource)
         {
             return x =>
-                (string.IsNullOrEmpty(searchResource.PartNumber) || x.PartNumber.ToUpper() == searchResource.PartNumber.ToUpper()) &&
-                (string.IsNullOrEmpty(searchResource.StorageTypeCode) || x.StorageTypeCode.ToUpper() == searchResource.StorageTypeCode.ToUpper());
+                x.PartNumber.StartsWith(searchResource.PartNumber) &&
+                (string.IsNullOrEmpty(searchResource.StorageTypeCode) || x.StorageTypeCode.Contains(searchResource.StorageTypeCode));
         }
 
         protected override Expression<Func<PartsStorageType, bool>> FindExpression(PartsStorageTypeResource searchResource)
