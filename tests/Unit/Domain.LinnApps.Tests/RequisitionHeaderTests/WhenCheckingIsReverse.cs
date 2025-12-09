4bfcd61bc5cea272d371c1eaca58ccac48f5cdb8
lewisrenfrew <Lewis.Renfrew@linn.co.uk>
2025-12-09 14:53:56 +0000

    allow changing qty on reversals (#327)
    * allow changing qty on reversals
    
    * qty check

--- tests/Unit/Domain.LinnApps.Tests/RequisitionHeaderTests/WhenCheckingIsReverse.cs
+++ tests/Unit/Domain.LinnApps.Tests/RequisitionHeaderTests/WhenCheckingIsReverse.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
-using Linn.Stores2.Domain.LinnApps.Accounts;
-using Linn.Stores2.Domain.LinnApps.Requisitions;
-using Linn.Stores2.TestData.FunctionCodes;
-using Linn.Stores2.TestData.Requisitions;
-using Linn.Stores2.TestData.Transactions;
-using NUnit.Framework;
-
-namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionHeaderTests
+﻿namespace Linn.Stores2.Domain.LinnApps.Tests.RequisitionHeaderTests
 {
+    using FluentAssertions;
+    using Linn.Stores2.Domain.LinnApps.Requisitions;
+    using Linn.Stores2.TestData.FunctionCodes;
+    using NUnit.Framework;
+
     public class WhenCheckingIsReverse 
     {
         private RequisitionHeader sut;
